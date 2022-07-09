29c0664d261d128f0e7a096ca2ca31f0381f38ac
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-09 13:05:27 +0900

    #30 React redux store 2

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import "./index.css";
 import "./styles/icons/icons.css";
 import App from "./App";
 import {BrowserRouter as Router} from "react-router-dom";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+import {composeWithDevTools} from "redux-devtools-extension";
 
 ReactDOM.render(
     <Router>
