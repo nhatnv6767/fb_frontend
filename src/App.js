160cb90579aa16a9c6b4b74fa5a00ffa9f823ff4
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-09 12:49:50 +0900

    #29 React router dom 9

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {Routes, Route} from "react-router-dom";
 import Login from "./pages/login";
+import Profile from "./pages/profile";
 
 function App() {
     return (
         <div>
             <Routes>
                 <Route path="/login" element={<Login/>} exact/>
+                <Route path="/profile" element={<Profile/>} exact/>
             </Routes>
         </div>
     );
