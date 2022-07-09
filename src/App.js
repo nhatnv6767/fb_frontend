270cda8f24920d2c31250d745de586fadb00a4c6
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-09 12:47:15 +0900

    #29 React router dom 8

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import {Routes, Route} from "react-router-dom";
+import Login from "./pages/login";
 
 function App() {
     return (
         <div>
             <Routes>
-                <Route path="/login"/>
+                <Route path="/login" element={<Login/>}/>
             </Routes>
         </div>
     );
