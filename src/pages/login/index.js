b6b69a950639f549bb55559ef53c774d1615aa26
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-09 13:47:10 +0900

    #31.1 Login and Register 2

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -4,7 +4,12 @@ export default function Login() {
     return (
         <div className="login">
             <div className="login_wrapper">
-                <div className="login_wrap"></div>
+                <div className="login_wrap">
+                    <div className="login_1">
+                        <img src="../../public/icons/facebook.svg" alt=""/>
+                    </div>
+                    <div className="login_2"></div>
+                </div>
                 <div className="register"></div>
             </div>
         </div>
