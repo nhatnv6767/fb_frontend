9520ce6d229a2b4c126d34ddc9952dc482c19778
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-09 22:00:19 +0900

    #37 Register Form 4

--- src/components/login/RegisterForm.js
+++ src/components/login/RegisterForm.js
@@ -1,5 +1,11 @@
 export default function RegisterForm() {
     return (
-        <div className="blur"></div>
+        <div className="blur">
+            <div className="register">
+                <div className="register_header">
+                    <i className="exit_icon"></i>
+                </div>
+            </div>
+        </div>
     );
 }
