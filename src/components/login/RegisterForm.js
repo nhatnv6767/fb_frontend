4b1a901bf30e4c055bb9e02b7492fc6823c7ea64
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-10 22:00:55 +0900

    #37 Register Form 19

--- src/components/login/RegisterForm.js
+++ src/components/login/RegisterForm.js
@@ -80,6 +80,43 @@ export default function RegisterForm() {
                                         </select>
                                     </div>
                                 </div>
+                                <div className="reg_col">
+                                    <div className="reg_line_header">
+                                        Gender <i className="info_icon"></i>
+                                    </div>
+                                    <div className="reg_grid">
+                                        <label htmlFor="male">
+                                            Male
+                                            <input
+                                                type="radio"
+                                                name="gender"
+                                                id="male"
+                                                value="male"
+                                                onChange={handleRegisterChange}
+                                            />
+                                        </label>
+                                        <label htmlFor="male">
+                                            Female
+                                            <input
+                                                type="radio"
+                                                name="gender"
+                                                id="female"
+                                                value="female"
+                                                onChange={handleRegisterChange}
+                                            />
+                                        </label>
+                                        <label htmlFor="male">
+                                            Custom
+                                            <input
+                                                type="radio"
+                                                name="gender"
+                                                id="custom"
+                                                value="custom"
+                                                onChange={handleRegisterChange}
+                                            />
+                                        </label>
+                                    </div>
+                                </div>
                             </Form>
                         )
                     }
