bfb7140e9da1cdfc1f995f11fe6360e308185e85
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-11 23:50:44 +0900

    #41 Register error handling 12

--- src/components/login/RegisterForm.js
+++ src/components/login/RegisterForm.js
@@ -100,8 +100,6 @@ export default function RegisterForm() {
                         let picked_date = new Date(bYear, bMonth - 1, bDay);
                         let atleast14 = new Date(1970 + 14, 0, 1);
                         let noMoreThan70 = new Date(1970 + 70, 0, 1);
-                        console.log("atleast14", atleast14);
-                        // AGE
                         if (current_date - picked_date < atleast14) {
                             setDateError(
                                 "It look like you're entered the wrong info. Please make sure that you use your real date of birth."
