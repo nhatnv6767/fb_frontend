6178cc2cdab9f367b260e869a5ae6600a1578874
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-12 02:31:30 +0900

    #42 Register submit 21

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,10 +1,9 @@
-/**
- * If the action type is "LOGIN", return the payload. Otherwise, return the state
- * @param [state=null] - the current state of the reducer.
- * @param action - This is the action object that was dispatched.
- * @returns The user object
- */
-export function userReducer(state = null, action) {
+import Cookies from "js-cookie";
+
+export function userReducer(
+    state = Cookies.get("user")
+        ? JSON.parse(Cookies.get("user")) : null,
+    action) {
     switch (action.type) {
         case "LOGIN":
             return action.payload;
