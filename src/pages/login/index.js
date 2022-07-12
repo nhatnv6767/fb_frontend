2a11a495186794fb621e32d8d8163219770c8e34
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-12 20:54:30 +0900

    #43 Login submit 1

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,9 +2,12 @@ import "./style.css";
 import LoginForm from "../../components/login/LoginForm";
 import Footer from "../../components/login/Footer";
 import RegisterForm from "../../components/login/RegisterForm";
+import {useState} from "react";
 
 
 export default function Login() {
+
+    const [visible, setVisible] = useState(false);
 
     return (
         <div className="login">
