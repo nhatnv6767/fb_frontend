aaf70e4f34e08943fedd29a6309f8548d8baf2d6
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-13 02:29:07 +0900

    #46 Click outside elements and close 5

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Header from "../../components/header";
+import useClickOutside from "../../helpers/clickOutside";
 
 export default function Home() {
+    useClickOutside();
     return (
         <div>
             <Header/>
