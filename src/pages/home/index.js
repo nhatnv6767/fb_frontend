6d26260a4a9cbcef34c59572118c33287126e765
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-12 21:48:29 +0900

    #44 Header base 3

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from "../../components/header";
 
 export default function Home() {
     return (
         <div>
-            Home
+            <Header/>
         </div>
     );
 }
