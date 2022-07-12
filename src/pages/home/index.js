c3df5a4adc0b5c04f01fd66f90cd5076902bda1e
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-13 02:50:29 +0900

    !!!!!!#46 Click outside elements and close 7

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import Header from "../../components/header";
 import useClickOutside from "../../helpers/clickOutside";
 
 export default function Home() {
-    useClickOutside();
+    const el = useRef(null);
+    useClickOutside(el, () => {
+        el.current.style.display = "none";
+    });
     return (
         <div>
             <Header/>
-            <div className="card"></div>
+            <div className="card" ref={el}></div>
         </div>
     );
 }
