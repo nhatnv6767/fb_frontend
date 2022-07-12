f91f031025d318fc60fdcc02b9a80a948f7aaec6
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-13 02:46:12 +0900

    !!!!!!

--- src/helpers/clickOutside.js
+++ src/helpers/clickOutside.js
@@ -2,7 +2,20 @@ import {useEffect} from "react";
 
 export default function useClickOutside(ref, fun) {
     useEffect(() => {
-        console.log("useEffect");
+        const listener = (e) => {
+            /* Checking if the click is outside of the ref. */
+            if (!ref.current || ref.current.contains(e.target)) {
+                return;
+            }
+            fun();
+        };
+        document.addEventListener("mousedown", listener);
+        document.addEventListener("touchstart", listener);
+
+        return () => {
+            document.removeEventListener("mousedown", listener);
+            document.removeEventListener("touchstart", listener);
+        };
     }, []);
 
 }
