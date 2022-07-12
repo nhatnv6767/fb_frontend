0624fdda58508b8e314e48335670abcb8d68466d
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-12 21:56:10 +0900

    #44 Header base 5

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,17 @@
 import "./style.css";
+import {Link} from "react-router-dom";
+import {Logo} from "../../svg";
 
 export default function Header() {
     return (
         <header>
-            <div className="header_left"></div>
+            <div className="header_left">
+                <Link to="/" className="header_logo">
+                    <div className="circle">
+                        <Logo/>
+                    </div>
+                </Link>
+            </div>
             <div className="header_middle"></div>
             <div className="header_right"></div>
         </header>
