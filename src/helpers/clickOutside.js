96bd83afaba87784f28b3ef1114a96fc72123600
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-13 02:26:09 +0900

    #46 Click outside elements and close 3

---
+++ src/helpers/clickOutside.js
@@ -0,0 +1,10 @@
+import {useEffect} from "react";
+
+export default function clickOutside(ref, fun) {
+    useEffect(() => {
+        return () => {
+            effect;
+        };
+    }, [input]);
+
+}
