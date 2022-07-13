018e326b780d0698028f4435a5adb789cf17397e
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-13 20:41:07 +0900

    !!!!!!#46 Click outside elements and close 11

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,9 +3,12 @@ import Header from "../../components/header";
 import useClickOutside from "../../helpers/clickOutside";
 
 export default function Home() {
-    const [visible, setVisible] = useState(false);
+    const [visible, setVisible] = useState(true);
     const el = useRef(null);
 
+    useClickOutside(el, () => {
+        setVisible(false);
+    });
     return (
         <div>
             <Header/>
