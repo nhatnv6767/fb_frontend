4733ebd364ca7a309a271003546e447791599d3e
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-13 20:32:46 +0900

    !!!!!!#46 Click outside elements and close 8 expalin

--- src/helpers/clickOutside.js
+++ src/helpers/clickOutside.js
@@ -6,6 +6,11 @@ export default function useClickOutside(ref, fun) {
     useEffect(() => {
         const listener = (e) => {
             /* Checking if the click is outside of the ref. */
+            /* `ref.current` is a reference to the element that we want to detect the outside click on. If `ref.current` is
+            `null`, then the element has not been rendered yet. */
+            /* `ref.current.contains(e.target)` is checking if the element that was clicked on is inside of the element
+            that we
+            want to detect the outside click on. */
             if (!ref.current || ref.current.contains(e.target)) {
                 return;
             }
@@ -20,6 +25,9 @@ export default function useClickOutside(ref, fun) {
             document.removeEventListener("mousedown", listener);
             document.removeEventListener("touchstart", listener);
         };
+        /* `[]` is the second argument to `useEffect`. It is an array of dependencies. If the array is empty, then the
+        function will only be called once. If the array is not empty, then the function will be called every time one of the
+        dependencies changes. */
     }, []);
 
 }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,12 +5,14 @@ import useClickOutside from "../../helpers/clickOutside";
 export default function Home() {
     const el = useRef(null);
     useClickOutside(el, () => {
+        /* `current` is a property of the `el` ref. It is a reference to the DOM element. */
         el.current.style.display = "none";
     });
     return (
         <div>
             <Header/>
             <div
+                /* A React hook that allows us to access the DOM element. */
                 className="card" ref={el}
             >
 
