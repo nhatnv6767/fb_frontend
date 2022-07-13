2070951b9819ccaecfd83ff72cde9d0d653869dd
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-13 20:39:28 +0900

    !!!!!!#46 Click outside elements and close 10

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,22 +1,22 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import Header from "../../components/header";
 import useClickOutside from "../../helpers/clickOutside";
 
 export default function Home() {
+    const [visible, setVisible] = useState(false);
     const el = useRef(null);
-    useClickOutside(el, () => {
-        /* `current` is a property of the `el` ref. It is a reference to the DOM element. */
-        el.current.style.display = "none";
-    });
+
     return (
         <div>
             <Header/>
-            <div
-                /* A React hook that allows us to access the DOM element. */
-                className="card" ref={el}
-            >
+            {visible &&
+                <div
+                    /* A React hook that allows us to access the DOM element. */
+                    className="card" ref={el}
+                >
 
-            </div>
+                </div>
+            }
         </div>
     );
 }
