60fd68baf1998026543ed924af6c341e203d1abd
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-14 19:17:04 +0900

    #48 All Menu 3

--- src/components/header/AllMenu.js
+++ src/components/header/AllMenu.js
@@ -1,7 +1,15 @@
 export default function AllMenu() {
     return (
-        <div>
-            AllMenu
+        <div className="all_menu">
+            <div className="all_menu_header">Menu</div>
+            <div className="all_menu_wrap scrollbar">
+                <div className="all_left">
+                    <div className="all_menu_search">
+                        <i className="amm_s_ic"></i>
+                        <input type="text" placeholder="Search Menu"/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
