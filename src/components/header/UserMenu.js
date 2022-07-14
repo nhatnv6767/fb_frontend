f54ddf40ee0877eb780c06e6b6cb1878a37ca6ab
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-14 23:26:13 +0900

    #49 User Menu 4

--- src/components/header/UserMenu.js
+++ src/components/header/UserMenu.js
@@ -5,6 +5,15 @@ export default function UserMenu({user}) {
         <div className="mmenu">
             <Link to="/profile" className="mmenu_header hover3">
                 <img src={user?.picture} alt=""/>
+                <div className="mmenu_col">
+                    <span>
+                        {user?.first_name}
+                        {user?.last_name}
+                    </span>
+                    <span>
+                        See your profile
+                    </span>
+                </div>
             </Link>
         </div>
     );
