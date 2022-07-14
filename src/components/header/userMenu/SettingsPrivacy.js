b57f26594f9af6365e0cc52915676e4be7a303ea
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-15 00:44:15 +0900

    #50 <2> User Menu 2

--- src/components/header/userMenu/SettingsPrivacy.js
+++ src/components/header/userMenu/SettingsPrivacy.js
@@ -13,6 +13,36 @@ export default function SettingsPrivacy() {
                 </div>
                 <span>Settings</span>
             </div>
+            <div className="mmenu_item hover3">
+                <div className="small_circle">
+                    <i className="privacy_checkup_icon"></i>
+                </div>
+                <span>Privacy Checkup</span>
+            </div>
+            <div className="mmenu_item hover3">
+                <div className="small_circle">
+                    <i className="privacy_shortcuts_icon"></i>
+                </div>
+                <span>Privacy Shorcuts</span>
+            </div>
+            <div className="mmenu_item hover3">
+                <div className="small_circle">
+                    <i className="activity_log_icon"></i>
+                </div>
+                <span>Activity log</span>
+            </div>
+            <div className="mmenu_item hover3">
+                <div className="small_circle">
+                    <i className="news_icon"></i>
+                </div>
+                <span>New Feed Preferences</span>
+            </div>
+            <div className="mmenu_item hover3">
+                <div className="small_circle">
+                    <i className="language_icon"></i>
+                </div>
+                <span>Language</span>
+            </div>
         </div>
     );
 }
