daf3dcccf678e54950d2c92b12bf5d1f517e21ec
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-14 23:31:24 +0900

    #49 User Menu 5

--- src/components/header/UserMenu.js
+++ src/components/header/UserMenu.js
@@ -15,6 +15,22 @@ export default function UserMenu({user}) {
                     </span>
                 </div>
             </Link>
+            <div className="mmenu_splitter"></div>
+            <div className="mmenu_main hover3">
+                <div className="small_circle">
+                    <i className="report_filled_icon"></i>
+                </div>
+                <div className="mmenu_col">
+                    <div className="mmenu_span1">Give feedback</div>
+                    <div className="mmenu_span2">Help us improve facebook</div>
+                </div>
+            </div>
+            <div className="mmenu_splitter"></div>
+            <div className="mmenu_item hover3">
+                <div className="small_circle">
+                    <div className="settings_filled_icon"></div>
+                </div>
+            </div>
         </div>
     );
 }
