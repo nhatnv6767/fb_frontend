279274831325af866af2e68a6e6e175520563759
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-15 02:27:16 +0900

    #53 Protected routes 4

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import {Routes, Route} from "react-router-dom";
 import Login from "./pages/login";
 import Profile from "./pages/profile";
 import Home from "./pages/home";
+import LoggedInRoutes from "./routes/LoggedInRoutes";
 
 function App() {
     return (
         <div>
             <Routes>
-
                 <Route path="/login" element={<Login/>} exact/>
-                <Route path="/profile" element={<Profile/>} exact/>
-                <Route path="/" element={<Home/>} exact/>
+                <Route element={<LoggedInRoutes/>}>
+                    <Route path="/profile" element={<Profile/>} exact/>
+                    <Route path="/" element={<Home/>} exact/>
+                </Route>
             </Routes>
         </div>
     );
