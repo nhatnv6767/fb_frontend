f205b0eb260c8cf2785296a55a71095dc6c07c61
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-15 20:17:07 +0900

    #54 Left home menu 5

--- src/routes/LoggedInRoutes.js
+++ src/routes/LoggedInRoutes.js
@@ -5,5 +5,6 @@ import Login from "../pages/login";
 export default function LoggedInRoutes() {
     // use Redux
     const {user} = useSelector((state) => ({...state}));
+    /* A component from `react-router-dom` that renders the component that matches the current route. */
     return user ? <Outlet/> : <Login/>;
 }
--- src/routes/NotLoggedInRoutes.js
+++ src/routes/NotLoggedInRoutes.js
@@ -3,6 +3,7 @@ import {Navigate} from "react-router-dom";
 import {Outlet} from "react-router-dom";
 
 export default function NotLoggedInRoutes() {
+    /* A hook that allows us to access the Redux store. */
     const {user} = useSelector((state) => ({...state}));
     return user ? <Navigate to="/"/> : <Outlet/>;
 }
