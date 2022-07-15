149c3e9c31806efc5605e8ebc312578d13361605
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-15 21:13:57 +0900

    #55 <2>Left home menu 4

--- src/components/home/left/index.js
+++ src/components/home/left/index.js
@@ -6,7 +6,7 @@ import {ArrowDown1} from "../../../svg";
 
 export default function LeftHome({user}) {
     return (
-        <div className="left_home">
+        <div className="left_home scrollbar">
             <Link to="/profile" className="left_link hover1">
                 <img src={user?.picture} alt=""/>
                 <span>{user?.first_name} {user?.last_name}</span>
