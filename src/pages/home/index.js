c32e19dc5cc477350428ece8b47fea8c94ba6da1
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-15 20:14:15 +0900

    #54 Left home menu 3

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,13 @@
 import React, {useRef, useState} from 'react';
 import Header from "../../components/header";
+import LeftHome from "../../components/home/left";
 
 export default function Home() {
 
     return (
         <div>
             <Header/>
-
+            <LeftHome/>
         </div>
     );
 }
