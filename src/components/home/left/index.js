faee58096bd2e9fe208586a518f0592ab24edf8f
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-15 21:21:31 +0900

    #55 <2>Left home menu 8

--- src/components/home/left/index.js
+++ src/components/home/left/index.js
@@ -3,8 +3,10 @@ import LeftLink from "./LeftLink";
 import {left} from "../../../data/home";
 import {Link} from "react-router-dom";
 import {ArrowDown1} from "../../../svg";
+import {useState} from "react";
 
 export default function LeftHome({user}) {
+    const [visible, setVisible] = useState(false);
     return (
         <div className="left_home scrollbar">
             <Link to="/profile" className="left_link hover1">
@@ -27,24 +29,28 @@ export default function LeftHome({user}) {
                 </div>
                 <span>See more</span>
             </div>
-            <div className="more_left">
-                {
-                    left.slice(8, left.length).map((link, i) => (
-                        <LeftLink
-                            key={i}
-                            img={link.img}
-                            text={link.text}
-                            notification={link.notification}
-                        />
-                    ))
-                }
-                <div className="left_link hover1">
-                    <div className="small_circle rotate360">
-                        <ArrowDown1/>
+            {
+                visible && (
+                    <div className="more_left">
+                        {
+                            left.slice(8, left.length).map((link, i) => (
+                                <LeftLink
+                                    key={i}
+                                    img={link.img}
+                                    text={link.text}
+                                    notification={link.notification}
+                                />
+                            ))
+                        }
+                        <div className="left_link hover1">
+                            <div className="small_circle rotate360">
+                                <ArrowDown1/>
+                            </div>
+                            <span>Show less</span>
+                        </div>
                     </div>
-                    <span>Show less</span>
-                </div>
-            </div>
+                )
+            }
         </div>
     );
 }
