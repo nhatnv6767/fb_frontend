7b9fc152763f0241245d511836df6d91972a6803
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-15 20:21:03 +0900

    #54 Left home menu 8

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,13 +1,14 @@
 import React, {useRef, useState} from 'react';
 import Header from "../../components/header";
 import LeftHome from "../../components/home/left";
+import {useSelector} from "react-redux";
 
 export default function Home() {
-
+    const {user} = useSelector((user) => ({...user}));
     return (
         <div>
             <Header/>
-            <LeftHome/>
+            <LeftHome user={user}/>
         </div>
     );
 }
