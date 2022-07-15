2d083032de642662a240e77292adbd8374244a56
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-15 21:05:07 +0900

    #54 Left home menu 19

--- src/components/home/left/index.js
+++ src/components/home/left/index.js
@@ -1,14 +1,15 @@
 import "./style.css";
 import LeftLink from "./LeftLink";
 import {left} from "../../../data/home";
+import {Link} from "react-router-dom";
 
 export default function LeftHome({user}) {
     return (
         <div className="left_home">
-            <div className="left_link hover1">
+            <Link to="/profile" className="left_link hover1">
                 <img src={user?.picture} alt=""/>
                 <span>{user?.first_name} {user?.last_name}</span>
-            </div>
+            </Link>
             {
                 left.slice(0, 8).map((link, i) => (
                     <LeftLink
