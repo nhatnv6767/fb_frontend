fb4a13e622f91bca363ac61392deded706b5b5ff
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-15 20:25:37 +0900

    #54 Left home menu 9

--- src/components/home/left/index.js
+++ src/components/home/left/index.js
@@ -1,10 +1,10 @@
 import "./style.css";
 
-export default function LeftHome() {
+export default function LeftHome({user}) {
     return (
         <div className="left_home">
             <div className="left_link">
-                <img src="" alt=""/>
+                <img src={user?.picture} alt=""/>
             </div>
         </div>
     );
