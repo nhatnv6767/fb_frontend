2cebb8b0a031a23befee1d8332dd165a0ea5ddda
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-17 02:03:18 +0900

    #70 Reset password 1

--- src/pages/reset/index.js
+++ src/pages/reset/index.js
@@ -13,11 +13,11 @@ export default function Reset() {
     const {user} = useSelector((state) => ({...state}));
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const {email, setEmail} = useState("");
-    const {code, setCode} = useState("");
-    const {password, setPassword} = useState("");
-    const {conf_password, setConf_password} = useState("");
-    const {error, setError} = useState("");
+    const [email, setEmail] = useState("");
+    const [code, setCode] = useState("");
+    const [password, setPassword] = useState("");
+    const [conf_password, setConf_password] = useState("");
+    const [error, setError] = useState("");
     const [visible, setVisible] = useState(0);
     const logout = () => {
         Cookies.set('user', "");
