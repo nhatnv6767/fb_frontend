cb733b7d2cd8a0f80a1935b2eb63037115c5daa5
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-16 20:48:50 +0900

    #65 re-Send verification code 6

--- src/components/home/sendVerification/index.js
+++ src/components/home/sendVerification/index.js
@@ -1,9 +1,18 @@
 import "./style.css";
+import {useState} from "react";
 
 export default function SendVerification() {
+    const [success, setSuccess] = useState("");
+    const [error, setError] = useState("");
     return (
-        <div>
-            SendVerification
+        <div className="send_verification">
+            <span>
+                Your account is not verified, verify your account before it gets deleted
+                after a month from creating.
+            </span>
+            <a href="">
+                click here to resend verification link
+            </a>
         </div>
     );
 }
