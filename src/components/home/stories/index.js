13e308480962d3d60e2f2b08821c8d36b05b6883
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-16 10:44:59 +0900

    #57 Stories 3

--- src/components/home/stories/index.js
+++ src/components/home/stories/index.js
@@ -1,10 +1,15 @@
 import "./style.css";
+import {Plus} from "../../../svg";
 
 export default function Stories() {
     return (
         <div className="stories">
             <div className="create_story_card">
-                <img src="../../../images/default_pic.png" alt=""/>
+                <img src="../../../images/default_pic.png" alt="" className="create_story_img"/>
+                <div className="plus_story">
+                    <Plus color="#fff"/>
+                </div>
+                <div className="story_create_text">Create Story</div>
             </div>
         </div>
     );
