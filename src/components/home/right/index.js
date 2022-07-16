9dcc10e499c10acc1f8ff06deb5114de5d400cde
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-16 09:19:24 +0900

    #56 Right home side 4

--- src/components/home/right/index.js
+++ src/components/home/right/index.js
@@ -1,9 +1,21 @@
 import "./style.css";
+import {NewRoom} from "../../../svg";
 
 export default function RightHome() {
     return (
-        <div>
-            RightHome
+        <div className="right_home">
+            <div className="heading">Sponsored</div>
+            <div className="splitter1"></div>
+            <div className="contacts_wrap">
+                <div className="contacts_header">
+                    <div className="contacts_header_left">Contacts</div>
+                    <div className="contacts_header_right">
+                        <div className="contact_circle">
+                            <NewRoom/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
