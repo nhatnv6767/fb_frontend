239b1dbfc724dd76e56114bcf108739689f0362d
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-16 14:25:19 +0900

    #62 Activate account 6

--- src/pages/home/ActivateForm.js
+++ src/pages/home/ActivateForm.js
@@ -1,7 +1,15 @@
-export default function ActivateForm() {
+export default function ActivateForm({type, header, text, loading}) {
     return (
-        <div>
-            ActivateForm
+        <div className="blur">
+            <div className="popup">
+                <div className="popup_header">
+                    Account verification succeeded
+                </div>
+                <div className="popup_message">
+                    Account verification succeeded
+                </div>
+            </div>
+
         </div>
     );
 }
