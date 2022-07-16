13bf0d705f798ace3d4a2881572d33b20a2fff51
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-16 21:02:12 +0900

    #65 re-Send verification code 10

--- src/components/home/sendVerification/index.js
+++ src/components/home/sendVerification/index.js
@@ -4,13 +4,20 @@ import {useState} from "react";
 export default function SendVerification() {
     const [success, setSuccess] = useState("");
     const [error, setError] = useState("");
+    const sendVerificationLink = async () => {
+
+    };
     return (
         <div className="send_verification">
             <span>
                 Your account is not verified, verify your account before it gets deleted
                 after a month from creating.
             </span>
-            <a href="">
+            <a
+                onClick={() => {
+                    sendVerificationLink();
+                }}
+            >
                 click here to resend verification link
             </a>
 
