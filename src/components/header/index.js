36a7e7a94be14f24bafdea331f86589a7110ba3b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-16 10:02:26 +0900

    #56 Right home side 14

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -100,7 +100,9 @@ export default function Header() {
                             setShowAllMenu(prev => !prev);
                         }}
                     >
-                        <Menu/>
+                        <div style={{transform: "translateY(2px)"}}>
+                            <Menu/>
+                        </div>
                     </div>
 
                     {
@@ -124,7 +126,9 @@ export default function Header() {
                             setShowUserMenu(prev => !prev);
                         }}
                     >
-                        <ArrowDown/>
+                        <div style={{transform: "translateY(2px)"}}>
+                            <ArrowDown/>
+                        </div>
                     </div>
                     {showUserMenu &&
                         <UserMenu user={user}/>
