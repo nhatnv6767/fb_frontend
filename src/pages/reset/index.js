b8e9bc19bf61d042a2e65d61f6a145495a1030b9
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-16 22:42:12 +0900

    #67 Reset password 9

--- src/pages/reset/index.js
+++ src/pages/reset/index.js
@@ -1,14 +1,25 @@
 import "./style.css";
 import {Link} from "react-router-dom";
+import {useSelector} from "react-redux";
 
 export default function Reset() {
+    const {user} = useSelector((state) => ({...state}));
     return (
         <div className="reset">
             <div className="reset_header">
                 <img src="../../../icons/facebook.svg" alt=""/>
-                <Link to="/login" className="right_reset">
-                    <button className="blue_btn">Login</button>
-                </Link>
+                {
+                    user ?
+                        <div className="right_reset">
+                            <Link to="/profile">
+                                <img src={user?.picture} alt=""/>
+                            </Link>
+                            <button className="blue_btn">Logout</button>
+                        </div>
+                        : <Link to="/login" className="right_reset">
+                            <button className="blue_btn">Login</button>
+                        </Link>
+                }
             </div>
         </div>
     );
