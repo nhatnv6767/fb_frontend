f761334e5cd91d21f349f0f79534bfb6669183ed
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-17 02:20:31 +0900

    #70 Reset password 4

--- src/pages/reset/ChangePassword.js
+++ src/pages/reset/ChangePassword.js
@@ -15,7 +15,10 @@ export default function ChangePassword({
         password: Yup.string()
             .required("Enter a combination of at least six numbers, letters and punctuation marks (such as ! and &).")
             .min(6, "Password must be at least 6 characters.")
-            .max(36, "Password can't be more than 36 characters.")
+            .max(36, "Password can't be more than 36 characters."),
+        conf_password: Yup.string().required("Confirm your password.")
+            .oneOf([Yup.ref("password")], "Passwords must match.")
+
     });
     return (
         <div className="reset_form" style={{height: "310px"}}>
