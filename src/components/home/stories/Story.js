cd15743d3d357250bdb6c5a39d8b136547ef8896
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-16 10:53:25 +0900

    #57 Stories 7

--- src/components/home/stories/Story.js
+++ src/components/home/stories/Story.js
@@ -1,7 +1,13 @@
-export default function Story() {
+export default function Story({story}) {
     return (
-        <div>
-            Story
+        <div className="story">
+            <img src={story.image} alt="" className="story_img"/>
+            <div className="story_profile_pic">
+                <img src={story.profile_picture} alt=""/>
+            </div>
+            <div className="story_profile_name">
+                {story.profile_name}
+            </div>
         </div>
     );
 }
