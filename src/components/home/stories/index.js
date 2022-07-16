7c98996a1b1b33c7a894d403b911591360fccd17
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-16 12:19:26 +0900

    #59 Responsive home 4

--- src/components/home/stories/index.js
+++ src/components/home/stories/index.js
@@ -2,8 +2,15 @@ import "./style.css";
 import {ArrowRight, Plus} from "../../../svg";
 import {stories} from "../../../data/home";
 import Story from "./Story";
+import {useMediaQuery} from "react-responsive";
 
 export default function Stories() {
+    /* A hook that allows you to use media queries in react. */
+    const query1175px = useMediaQuery({
+        /* A media query that is being used to determine the size of the screen. */
+        query: "(max-width: 1175px)",
+    });
+    const max = query1175px ? 4 : stories.length;
     return (
         <div className="stories">
             <div className="create_story_card">
@@ -14,7 +21,7 @@ export default function Stories() {
                 <div className="story_create_text">Create Story</div>
             </div>
             {
-                stories.map((story, i) => (
+                stories.slice(0, max).map((story, i) => (
                     <Story story={story}/>
                 ))
             }
