71711c93c3360efdc55eff67f28378eb8d3e59e9
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-16 11:46:06 +0900

    #58 Create Post 4

--- src/components/createPost/index.js
+++ src/components/createPost/index.js
@@ -1,11 +1,15 @@
 import "./style.css";
 
-export default function CreatePost() {
+export default function CreatePost({user}) {
     return (
         <div className="createPost">
             <div className="createPost_header">
-                <img src="" alt=""/>
+                <img src={user?.picture} alt=""/>
+                <div className="open_post">
+                    What's on your mind, {user?.first_name}
+                </div>
             </div>
+            <div className="create_splitter"></div>
         </div>
     );
 }
