c9348e6181c70a6b59d0db12b9468decae36b06b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-16 22:31:57 +0900

    #67 Reset password 6

--- src/pages/reset/index.js
+++ src/pages/reset/index.js
@@ -1,10 +1,14 @@
 import "./style.css";
+import {Link} from "react-router-dom";
 
 export default function Reset() {
     return (
         <div className="reset">
             <div className="reset_header">
                 <img src="../../../icons/facebook.svg" alt=""/>
+                <Link to="/login" className="right_reset">
+                    <button className="blue_btn">Login</button>
+                </Link>
             </div>
         </div>
     );
