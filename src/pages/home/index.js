a3c3b638d66dcecebac6faaa9f9a9e907fc375b7
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-16 20:45:08 +0900

    #65 re-Send verification code 5

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,6 +6,7 @@ import RightHome from "../../components/home/right";
 import Stories from "../../components/home/stories";
 import "./style.css";
 import CreatePost from "../../components/createPost";
+import SendVerification from "../../components/home/sendVerification";
 
 export default function Home() {
     const {user} = useSelector((user) => ({...user}));
@@ -15,6 +16,7 @@ export default function Home() {
             <LeftHome user={user}/>
             <div className="home_middle">
                 <Stories/>
+                <SendVerification/>
                 <CreatePost user={user}/>
             </div>
             <RightHome user={user}/>
