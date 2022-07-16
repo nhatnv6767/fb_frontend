1114df406bce96316eafeb4f8b891db809d50868
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-16 13:01:08 +0900

    #56 Responsive home 2

--- src/components/home/stories/index.js
+++ src/components/home/stories/index.js
@@ -14,7 +14,11 @@ export default function Stories() {
     const query1030px = useMediaQuery({
         query: "(max-width: 1030px)",
     });
-    const max = query1030px ? 5 : query1175px ? 4 : stories.length;
+
+    const query960px = useMediaQuery({
+        query: "(max-width: 960px)",
+    });
+    const max = query960px ? 4 : query1030px ? 5 : query1175px ? 4 : stories.length;
     return (
         <div className="stories">
             <div className="create_story_card">
