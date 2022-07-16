47c71dc4e23ed197bc688c11effab3381fa847f5
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-16 14:22:59 +0900

    #62 Activate account 5

--- src/pages/home/activate.js
+++ src/pages/home/activate.js
@@ -6,6 +6,7 @@ import RightHome from "../../components/home/right";
 import Stories from "../../components/home/stories";
 import "./style.css";
 import CreatePost from "../../components/createPost";
+import ActivateForm from "./ActivateForm";
 
 export default function Activate() {
     const {user} = useSelector((user) => ({...user}));
@@ -14,6 +15,12 @@ export default function Activate() {
     const [loading, setLoading] = useState(false);
     return (
         <div className="home">
+            <ActivateForm
+                type="success"
+                header="Account verification succeeded"
+                text={success}
+                loading={loading}
+            />
             <Header/>
             <LeftHome user={user}/>
             <div className="home_middle">
