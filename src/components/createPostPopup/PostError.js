ccc5f9dc19e4aa14cd7733d0fc76a1be127bca27
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-19 02:01:11 +0900

    #87 Error Popup, error handling 8

--- src/components/createPostPopup/PostError.js
+++ src/components/createPostPopup/PostError.js
@@ -2,7 +2,10 @@ export default function PostError({error, setError}) {
     return (
         <div className="postError">
             <div>{error}</div>
-            <button className="blue_btn">Try again</button>
+            <button className="blue_btn" onClick={() => {
+                setError("");
+            }}>Try again
+            </button>
         </div>
     );
 }
