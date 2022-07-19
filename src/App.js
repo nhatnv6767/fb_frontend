ec142e0307eca28ac4c2bd66006a41a3cdccb5d5
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-19 21:03:08 +0900

    #90 Display all post 5

--- src/App.js
+++ src/App.js
@@ -14,6 +14,17 @@ function reducer(state, action) {
     switch (action.type) {
         case "POSTS_REQUEST":
             return {...state, loading: true, error: ""};
+        case "POSTS_SUCCESS":
+            return {
+                ...state,
+                loading: false,
+                posts: action.payload,
+                error: ""
+            }
+        case "POSTS_ERROR":
+            return {...state, loading: false, error: action.payload}
+        default:
+            return state;
     }
 }
 
