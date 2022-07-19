f06a962b28e043ecc081ce45c0f308da3afa9b05
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-19 21:53:21 +0900

    #91 Post component-header 9

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -7,6 +7,11 @@ export default function Post({post}) {
             <div className="post_header">
                 <Link to={`/profile/${post.user.username}`} className="post_header_left">
                     <img src={post.user.picture} alt=""/>
+                    <div className="header_col">
+                        <div className="post_profile_name">
+                            {post.user.first_name} {post.user.last_name}
+                        </div>
+                    </div>
                 </Link>
             </div>
         </div>
