b61ca83799e99ea55a5500d6389864ca25b1e6d9
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-21 01:02:34 +0900

    #95 Create comment, emoji, image 24

--- src/components/post/CreateComment.js
+++ src/components/post/CreateComment.js
@@ -4,7 +4,7 @@ import Picker from "emoji-picker-react";
 export default function CreateComment({user}) {
     const [picker, setPicker] = useState(false);
     const [text, setText] = useState("");
-    const [error, setError] = useState("");
+    const [error, setError] = useState("format is not supported.");
     const [commentImage, setCommentImage] = useState("");
     const [cursorPosition, setCursorPosition] = useState();
     const textRef = useRef(null);
@@ -72,15 +72,21 @@ export default function CreateComment({user}) {
                         accept="image/jpeg, image/png, image/gif, image/webp"
                         onChange={handleImage}
                     />
-                    <div className="postError comment_error">
-                        <div className="postError_error">{error}</div>
-                        <button
-                            className="blue_btn"
-                            onClick={() => setError("")}
-                        >
-                            Try again
-                        </button>
-                    </div>
+
+                    {
+                        error && (
+                            <div className="postError comment_error">
+                                <div className="postError_error">{error}</div>
+                                <button
+                                    className="blue_btn"
+                                    onClick={() => setError("")}
+                                >
+                                    Try again
+                                </button>
+                            </div>
+                        )
+                    }
+
                     <input
                         type="text"
                         ref={textRef}
