575b91a41b358f0cfc4e99a6d481a59ea20efa15
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-20 21:07:02 +0900

    #94 Post reacts and actions 9

--- src/components/post/ReactsPopup.js
+++ src/components/post/ReactsPopup.js
@@ -1,4 +1,30 @@
 export default function ReactsPopup() {
+    const reactsArray = [
+        {
+            name: "like",
+            image: "../../../reacts/like.gif",
+        },
+        {
+            name: "love",
+            image: "../../../reacts/love.gif",
+        },
+        {
+            name: "haha",
+            image: "../../../reacts/haha.gif",
+        },
+        {
+            name: "wow",
+            image: "../../../reacts/wow.gif",
+        },
+        {
+            name: "sad",
+            image: "../../../reacts/sad.gif",
+        },
+        {
+            name: "angry",
+            image: "../../../reacts/angry.gif",
+        },
+    ];
     return (
         <div>
             ReactsPopup
