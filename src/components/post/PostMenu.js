0bf0b4fd09db3166b7a0562911a67297088c23f6
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-21 01:22:02 +0900

    #96 Post Menu 5

--- src/components/post/PostMenu.js
+++ src/components/post/PostMenu.js
@@ -1,6 +1,12 @@
+import MenuItem from "./MenuItem";
+
 export default function PostMenu() {
     return (
         <ul className="post_menu">
+            <MenuItem
+                icon="pin_icon"
+                title="Pin Post"
+            />
         </ul>
     );
 }
