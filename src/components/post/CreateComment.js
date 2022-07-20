cccb5d29fafabd1c8e3586270b333a9e33c1a045
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-21 00:05:11 +0900

    #95 Create comment, emoji, image 6

--- src/components/post/CreateComment.js
+++ src/components/post/CreateComment.js
@@ -1,7 +1,28 @@
-import {useState} from "react";
+import {useState, useEffect, useRef} from "react";
 
 export default function CreateComment({user}) {
     const [picker, setPicker] = useState(false);
+    const [text, setText] = useState(false);
+    const [cursorPosition, setCursorPosition] = useState();
+    const textRef = useRef(null);
+    useEffect(() => {
+        /* Setting the cursor position to the end of the text. */
+        textRef.current.selectionEnd = cursorPosition;
+    }, [cursorPosition]);
+
+    const handleEmoji = (e, {emoji}) => {
+        const ref = textRef.current;
+        /* Focusing on the textarea. */
+        ref.focus();
+        /* Getting the text before the cursor. */
+        const start = text.substring(0, ref.selectionStart);
+        // ! <DEBUG HERE>
+        const end = text.substring(ref.selectionStart);
+        const newText = start + emoji + end;
+        setText(newText);
+        /* Setting the cursor position to the end of the text. */
+        setCursorPosition(start.length + emoji.length);
+    };
     return (
         <div className="create_comment_wrap">
             <div className="create_comment">
