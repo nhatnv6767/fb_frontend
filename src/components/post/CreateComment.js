d6810d16bbf4bf9b1468bda11d94b4ff33fc24bf
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-21 01:03:31 +0900

    #95 Create comment, emoji, image 25

--- src/components/post/CreateComment.js
+++ src/components/post/CreateComment.js
@@ -4,7 +4,7 @@ import Picker from "emoji-picker-react";
 export default function CreateComment({user}) {
     const [picker, setPicker] = useState(false);
     const [text, setText] = useState("");
-    const [error, setError] = useState("format is not supported.");
+    const [error, setError] = useState("");
     const [commentImage, setCommentImage] = useState("");
     const [cursorPosition, setCursorPosition] = useState();
     const textRef = useRef(null);
