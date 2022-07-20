208f9e31c99d5f8a37829d8dd00725195b5f8823
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-21 01:20:30 +0900

    #96 Post Menu 4

--- src/components/post/MenuItem.js
+++ src/components/post/MenuItem.js
@@ -2,6 +2,16 @@ export default function MenuItem({icon, title, subtitle}) {
     return (
         <li className="hover1">
             <i className={icon}></i>
+            <div className="post_menu_text">
+                <span>{title}</span>
+                {
+                    subtitle && (
+                        <span className="menu_post_col">
+                            {subtitle}
+                        </span>
+                    )
+                }
+            </div>
         </li>
     );
 }
