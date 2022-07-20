b445a7cbfd12fad28313a9ec1650d55613ef9f0b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-20 23:23:27 +0900

    #94 Post reacts and actions 13

--- src/components/post/ReactsPopup.js
+++ src/components/post/ReactsPopup.js
@@ -1,39 +1,48 @@
+import {useState} from "react";
+
+const reactsArray = [
+    {
+        name: "like",
+        image: "../../../reacts/like.gif",
+    },
+    {
+        name: "love",
+        image: "../../../reacts/love.gif",
+    },
+    {
+        name: "haha",
+        image: "../../../reacts/haha.gif",
+    },
+    {
+        name: "wow",
+        image: "../../../reacts/wow.gif",
+    },
+    {
+        name: "sad",
+        image: "../../../reacts/sad.gif",
+    },
+    {
+        name: "angry",
+        image: "../../../reacts/angry.gif",
+    },
+];
 export default function ReactsPopup() {
-    const reactsArray = [
-        {
-            name: "like",
-            image: "../../../reacts/like.gif",
-        },
-        {
-            name: "love",
-            image: "../../../reacts/love.gif",
-        },
-        {
-            name: "haha",
-            image: "../../../reacts/haha.gif",
-        },
-        {
-            name: "wow",
-            image: "../../../reacts/wow.gif",
-        },
-        {
-            name: "sad",
-            image: "../../../reacts/sad.gif",
-        },
-        {
-            name: "angry",
-            image: "../../../reacts/angry.gif",
-        },
-    ];
+    const [visible, setVisible] = useState(false);
     return (
-        <div className="reacts_popup">
+        <>
             {
-                reactsArray.map((react, i) => (
-                    <div className="react" key={i}>
-                        <img src={react.image} alt=""/>
+                visible && (
+                    <div className="reacts_popup">
+                        {
+                            reactsArray.map((react, i) => (
+                                <div className="react" key={i}>
+                                    <img src={react.image} alt=""/>
+                                </div>
+                            ))
+                        }
                     </div>
-                ))
+                )
             }
-        </div>
+        </>
     );
 }
