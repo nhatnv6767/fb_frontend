cc77ece0f8ec2a88a791fee31d0e20a880be11f6
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-21 01:27:48 +0900

    #96 Post Menu 7

--- src/components/post/PostMenu.js
+++ src/components/post/PostMenu.js
@@ -1,12 +1,18 @@
 import MenuItem from "./MenuItem";
+import {useState} from "react";
 
 export default function PostMenu({postUserId, userId}) {
+    const [test, setTest] = useState(postUserId === userId ? true : false);
     return (
         <ul className="post_menu">
-            <MenuItem
-                icon="pin_icon"
-                title="Pin Post"
-            />
+            {
+                test && (
+                    <MenuItem
+                        icon="pin_icon"
+                        title="Pin Post"
+                    />
+                )
+            }
         </ul>
     );
 }
