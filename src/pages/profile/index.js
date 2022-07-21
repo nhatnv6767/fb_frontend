3a7be32f756ced4f21ea89273566ed9e150bc117
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-21 21:05:16 +0900

    #99 Profile frontend setup and get profile details from backend 3

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import {useParams} from "react-router-dom";
+import {useSelector} from "react-redux";
 
 export default function Profile() {
     const {username} = useParams();
-    console.log(username);
+    const {user} = useSelector((state) => ({...state}));
+    var userName = username === undefined ? user.username : username;
+    console.log(userName);
     return (
         <div>
             Profile
