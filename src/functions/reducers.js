9749d7f22ee15493df8460f4d3dc0318c4f15552
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-21 21:21:59 +0900

    #99 Profile frontend setup and get profile details from backend 5

--- src/functions/reducers.js
+++ src/functions/reducers.js
@@ -15,3 +15,21 @@ export function postsReducer(state, action) {
             return state;
     }
 }
+
+export function profileReducer(state, action) {
+    switch (action.type) {
+        case "PROFILE_REQUEST":
+            return {...state, loading: true, error: ""};
+        case "PROFILE_SUCCESS":
+            return {
+                ...state,
+                loading: false,
+                posts: action.payload,
+                error: ""
+            };
+        case "PROFILE_ERROR":
+            return {...state, loading: false, error: action.payload};
+        default:
+            return state;
+    }
+}
