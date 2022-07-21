70c009bcdab3701ae07b6f3924aa4829f63551af
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-21 21:00:08 +0900

    #99 Profile frontend setup and get profile details from backend 2

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import {useParams} from "react-router-dom";
 
 export default function Profile() {
+    const {username} = useParams();
+    console.log(username);
     return (
         <div>
             Profile
