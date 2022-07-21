c813d0d48c27c74f79a5037791765f8e5b520291
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-21 21:48:13 +0900

    #100 Profile header and cover section 4

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -66,6 +66,8 @@ export default function Header({page}) {
             }
 
             <div className="header_middle">
+                {/* A ternary operator. It is used to check if the page is home or not. If it is home then it will add the
+                class active to the element. */}
                 <Link to="/" className={`middle_icon ${page === "home" ? "active" : ""} `}>
                     {
                         page === "home" ? <HomeActive color={color}/> : <Home color={color}/>
