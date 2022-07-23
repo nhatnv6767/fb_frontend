259e9b9a8ba92d01e4ad061a102afa0e490133a4
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-23 21:20:11 +0900

    #102 Profile people you may know 5

--- src/pages/profile/PplYouMayKnow.js
+++ src/pages/profile/PplYouMayKnow.js
@@ -1,7 +1,16 @@
+import {Dots} from "../../svg";
+
 export default function PplYouMayKnow() {
     return (
-        <div>
-            PplYouMayKnow
+        <div className="pplumayknow">
+            <div className="pplumayknow_header">
+                People You May Know
+                <div className="post_header_right ppl_circle">
+                    <Dots/>
+                </div>
+            </div>
+            <div className="pplumayknow_list">
+            </div>
         </div>
     );
 }
