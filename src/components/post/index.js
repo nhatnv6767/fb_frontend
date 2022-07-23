ad30048cf4d7543b2bd2054544e5ff832b12868f
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-24 00:20:56 +0900

    #105 Photos, Friends, Footer components 2

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -12,7 +12,7 @@ export default function Post({post, user, profile}) {
     const [visible, setVisible] = useState(false);
     const [showMenu, setShowMenu] = useState(false);
     return (
-        <div className="post">
+        <div className="post" style={{width: `${profile && "100%"}`}}>
             <div className="post_header">
                 <Link
                     to={`/profile/${post.user.username}`}
