6e0ed7414530684f141696e459f389015a9fa039
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-24 00:06:43 +0900

    #104 Display profile posts and check if the profile is yours or a visitor 16

--- src/pages/profile/ProfilePictureInfos.js
+++ src/pages/profile/ProfilePictureInfos.js
@@ -1,4 +1,4 @@
-export default function ProfilePictureInfos({profile}) {
+export default function ProfilePictureInfos({profile, visitor}) {
     return (
         <div className="profile_img_wrap">
             <div className="profile_w_left">
@@ -21,16 +21,20 @@ export default function ProfilePictureInfos({profile}) {
                     <div className="profile_friend_imgs"></div>
                 </div>
             </div>
-            <div className="profile_w_right">
-                <div className="blue_btn">
-                    <img src="../../../icons/plus.png" alt="" className="invert"/>
-                    <span>Add to story</span>
-                </div>
-                <div className="gray_btn">
-                    <i className="edit_icon"></i>
-                    <span>Edit profile</span>
-                </div>
-            </div>
+            {
+                visitor ? ("") : (
+                    <div className="profile_w_right">
+                        <div className="blue_btn">
+                            <img src="../../../icons/plus.png" alt="" className="invert"/>
+                            <span>Add to story</span>
+                        </div>
+                        <div className="gray_btn">
+                            <i className="edit_icon"></i>
+                            <span>Edit profile</span>
+                        </div>
+                    </div>
+                )
+            }
         </div>
     );
 }
--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -69,6 +69,7 @@ export default function Profile({setVisible}) {
                     />
                     <ProfilePictureInfos
                         profile={profile}
+                        visitor={visitor}
                     />
                     <ProfileMenu/>
                 </div>
