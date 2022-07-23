6b7df1eb689c9d7784ec257b5c0675b933a9240b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-24 00:36:53 +0900

    #105 Photos, Friends, Footer components 7

--- src/pages/profile/Photos.js
+++ src/pages/profile/Photos.js
@@ -1,4 +1,46 @@
+import {useEffect, useReducer} from "react";
+import {profileReducer} from "../../functions/reducers";
+import axios from "axios";
+
 export default function Photos() {
+    const [{loading, error, profile}, dispatch] = useReducer(profileReducer, {
+        loading: false,
+        profile: {},
+        error: ""
+    });
+
+    useEffect(() => {
+        getProfile();
+    }, [userName]);
+    // var visitor = userName === user.username ? false : true;
+    var visitor = userName !== user.username;
+    console.log(visitor);
+    const getProfile = async () => {
+        try {
+            dispatch({
+                type: "PROFILE_REQUEST",
+            });
+            const {data} = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/getProfile/${userName}`, {
+                headers: {
+                    Authorization: `Bearer ${user.token}`,
+                }
+            });
+            if (data.ok === false) {
+                navigate("/profile");
+            } else {
+                dispatch({
+                    type: "PROFILE_SUCCESS",
+                    payload: data,
+                });
+            }
+
+        } catch (e) {
+            dispatch({
+                type: "PROFILE_ERROR",
+                payload: e.response.data.message,
+            });
+        }
+    };
     return (
         <div className="profile_card">
             <div className="profile_card_header">
