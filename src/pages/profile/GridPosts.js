41b5039b8cfdbd803b3913776897ebd1ae880430
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-23 22:51:04 +0900

    #103 Profile create post & post grid section 8

--- src/pages/profile/GridPosts.js
+++ src/pages/profile/GridPosts.js
@@ -1,5 +1,18 @@
 export default function GridPosts() {
     return (
-        <div>GridPosts</div>
+        <div className="createPost">
+            <div className="createPost_header">
+                <div className="left_header_grid">Posts</div>
+                <div className="flex">
+                    <div className="gray_btn">
+                        <i className="equalize_icon"></i>
+                    </div>
+                    <div className="gray_btn">
+                        <i className="manage_icon"></i>
+                        Manage Posts
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
