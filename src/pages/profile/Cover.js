974161cd5a1a24d94d689ab91ce198e4400575cd
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-24 00:05:00 +0900

    #104 Display profile posts and check if the profile is yours or a visitor 15

--- src/pages/profile/Cover.js
+++ src/pages/profile/Cover.js
@@ -11,29 +11,33 @@ export default function Cover({cover, visitor}) {
                 cover &&
                 <img src={cover} className="cover" alt=""/>
             }
-            <div className="update_cover_wrapper">
-                <div
-                    className="open_cover_update"
-                    onClick={() => setShowCoverMenu((prev) => !prev)}
-                >
-                    <i className="camera_filled_icon"></i>
-                    Add Cover Photo
-                </div>
-                {
-                    showCoverMenu && (
-                        <div className="open_cover_menu" ref={menuRef}>
-                            <div className="open_cover_menu_item hover1">
-                                <i className="photo_icon"></i>
-                                Select Photo
-                            </div>
-                            <div className="open_cover_menu_item hover1">
-                                <i className="upload_icon"></i>
-                                Upload Photo
-                            </div>
+            {
+                !visitor && (
+                    <div className="update_cover_wrapper">
+                        <div
+                            className="open_cover_update"
+                            onClick={() => setShowCoverMenu((prev) => !prev)}
+                        >
+                            <i className="camera_filled_icon"></i>
+                            Add Cover Photo
                         </div>
-                    )
-                }
-            </div>
+                        {
+                            showCoverMenu && (
+                                <div className="open_cover_menu" ref={menuRef}>
+                                    <div className="open_cover_menu_item hover1">
+                                        <i className="photo_icon"></i>
+                                        Select Photo
+                                    </div>
+                                    <div className="open_cover_menu_item hover1">
+                                        <i className="upload_icon"></i>
+                                        Upload Photo
+                                    </div>
+                                </div>
+                            )
+                        }
+                    </div>
+                )
+            }
         </div>
     );
 }
