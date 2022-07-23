f3e21cc5ae841c108bf9eaff0e724d9032b92082
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-23 21:42:45 +0900

    #102 Profile people you may know 9

--- src/pages/profile/AddFriendSmallCard.js
+++ src/pages/profile/AddFriendSmallCard.js
@@ -1,7 +1,16 @@
-export default function AddFriendSmallCard() {
+export default function AddFriendSmallCard({item}) {
     return (
-        <div>
-            AddFriendSmallCard
+        <div className="addfriendCard">
+            <div className="addfriend_imgsmall">
+                <img src={item.profile_picture} alt=""/>
+                <div className="addfriend_infos">
+                    <div className="addfriend_name">{item.profile_name}</div>
+                    <div className="light_blur_btn">
+                        <img src="../../../icons/addFriend.png" alt=""/>
+                        Add Friend
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
