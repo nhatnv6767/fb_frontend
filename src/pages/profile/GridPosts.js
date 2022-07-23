d462a96f24b9f03600a427a69ba6d580c5db0221
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-23 23:00:45 +0900

    #103 Profile create post & post grid section 11

--- src/pages/profile/GridPosts.js
+++ src/pages/profile/GridPosts.js
@@ -1,7 +1,10 @@
 export default function GridPosts() {
     return (
         <div className="createPost">
-            <div className="createPost_header">
+            <div
+                className="createPost_header"
+                style={{justifyContent: "space-between"}}
+            >
                 <div className="left_header_grid">Posts</div>
                 <div className="flex">
                     <div className="gray_btn">
