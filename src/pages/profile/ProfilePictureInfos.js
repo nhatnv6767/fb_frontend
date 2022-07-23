2b806317a4bcf30a9ad573617f32bb834580dd11
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-23 13:47:06 +0900

    #101 Profile picture, infos, menu 6

--- src/pages/profile/ProfilePictureInfos.js
+++ src/pages/profile/ProfilePictureInfos.js
@@ -2,10 +2,12 @@ export default function ProfilePictureInfos({profile}) {
     return (
         <div className="profile_img_wrap">
             <div className="profile_w_left">
-                <div className="profile_w_bg" style={{
-                    backgroundSize: "cover",
-                    backgroundImage: `url(${profile.picture})`
-                }}></div>
+                <div className="profile_w_img">
+                    <div className="profile_w_bg" style={{
+                        backgroundSize: "cover",
+                        backgroundImage: `url(${profile.picture})`
+                    }}></div>
+                </div>
             </div>
         </div>
     );
