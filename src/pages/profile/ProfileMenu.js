e6e280e28e3010bae36b3bed5022c7f5551a9fff
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-23 19:09:15 +0900

    #101 Profile picture, infos, menu 26

--- src/pages/profile/ProfileMenu.js
+++ src/pages/profile/ProfileMenu.js
@@ -5,12 +5,12 @@ export default function ProfileMenu() {
     return (
         <div className="profile_menu_wrap">
             <div className="profile_menu">
-                <Link to="/" className="active">Posts</Link>
+                <Link to="/" className="profile_menu_active">Posts</Link>
                 <Link to="/" className="hover1">About</Link>
                 <Link to="/" className="hover1">Friends</Link>
                 <Link to="/" className="hover1">Photos</Link>
                 <Link to="/" className="hover1">Videos</Link>
-                <Link to="/" className="hover1">Check_ins</Link>
+                <Link to="/" className="hover1">Check-ins</Link>
                 <Link to="/" className="hover1">More</Link>
                 <div className="p10_dots">
                     <Dots/>
