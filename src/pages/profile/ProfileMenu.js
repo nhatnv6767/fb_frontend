dda3cd5b61d24f0e0e857c6597b517f71ca9f745
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-23 18:57:23 +0900

    #101 Profile picture, infos, menu 24

--- src/pages/profile/ProfileMenu.js
+++ src/pages/profile/ProfileMenu.js
@@ -1,7 +1,21 @@
+import {Link} from "react-router-dom";
+import {Dots} from "../../svg";
+
 export default function ProfileMenu() {
     return (
-        <div>
-            ProfileMenu
+        <div className="profile_menu_wrap">
+            <div className="profile_menu">
+                <Link to="/" className="active">Posts</Link>
+                <Link to="/" className="hover1">About</Link>
+                <Link to="/" className="hover1">Friends</Link>
+                <Link to="/" className="hover1">Photos</Link>
+                <Link to="/" className="hover1">Videos</Link>
+                <Link to="/" className="hover1">Check_ins</Link>
+                <Link to="/" className="hover1">More</Link>
+                <div className="p10_dots">
+                    <Dots/>
+                </div>
+            </div>
         </div>
     );
 }
