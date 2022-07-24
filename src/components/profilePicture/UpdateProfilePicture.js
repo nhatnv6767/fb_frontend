d525c842fdd014e2bc7c8a74791047c0ef5a5f97
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-24 22:47:55 +0900

    #106<1> Update profile picture 17

--- src/components/profilePicture/UpdateProfilePicture.js
+++ src/components/profilePicture/UpdateProfilePicture.js
@@ -1,5 +1,12 @@
 export default function UpdateProfilePicture() {
     return (
-        <div>UpdateProfilePicture</div>
+        <div className="postBox update_img">
+            <div className="box_header">
+                <div className="small_circle">
+                    <i className="exit_icon"></i>
+                </div>
+                <span>Update profile picture</span>
+            </div>
+        </div>
     );
 }
