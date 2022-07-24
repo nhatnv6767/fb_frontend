2dcf3348b5db8afbc25f30189c546ce13bbdc5b0
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-24 21:37:19 +0900

    #105 Photos, Friends, Footer components 22

--- src/pages/profile/Friends.js
+++ src/pages/profile/Friends.js
@@ -8,13 +8,17 @@ export default function Friends({friends}) {
                     See all friends
                 </div>
             </div>
-            <div className="profile_card_count">
-                {
-                    friends.length === 0 ? ""
-                        : friends.length === 1 ? "1 friend"
-                            : `${friends.length} friends`
-                }
-            </div>
+            {
+                friends && (
+                    <div className="profile_card_count">
+                        {
+                            friends.length === 0 ? ""
+                                : friends.length === 1 ? "1 friend"
+                                    : `${friends.length} friends`
+                        }
+                    </div>
+                )
+            }
             <div className="profile_card_grid">
                 {
                     friends && friends.slice(0, 9).map((friend) => (
