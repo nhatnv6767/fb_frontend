7bf7d0989a5ae3a42880089262669d87ca7c1700
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-24 22:10:51 +0900

    #106<1> Update profile picture 4

--- src/components/profilePicture/index.js
+++ src/components/profilePicture/index.js
@@ -3,9 +3,24 @@ import {useRef} from "react";
 
 export default function ProfilePicture() {
     const refInput = useRef(null);
+    const handleImage = () => {
+    };
     return (
         <div className="blur">
-            <input type="file" ref={refInput}/>
+            <input
+                type="file"
+                ref={refInput}
+                hidden
+                onChange={handleImage}
+            />
+            <div className="postBox pictureBox">
+                <div className="box_header">
+                    <div className="box_circle">
+                        <i className="exit_icon"></i>
+                    </div>
+                    <span>Update profile picture</span>
+                </div>
+            </div>
         </div>
     );
 }
