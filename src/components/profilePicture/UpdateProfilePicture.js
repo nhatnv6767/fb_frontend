690ecce68d3427c3934181aced56d61f795a59e6
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-24 22:52:16 +0900

    #106<1> Update profile picture 21

--- src/components/profilePicture/UpdateProfilePicture.js
+++ src/components/profilePicture/UpdateProfilePicture.js
@@ -2,7 +2,7 @@ export default function UpdateProfilePicture({setImage}) {
     return (
         <div className="postBox update_img">
             <div className="box_header">
-                <div className="small_circle">
+                <div className="small_circle" onClick={() => setImage("")}>
                     <i className="exit_icon"></i>
                 </div>
                 <span>Update profile picture</span>
