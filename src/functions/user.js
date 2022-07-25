08dc5fbee6e1b0c061190e20b1de76f193432b38
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-25 22:27:13 +0900

    #107<3> Update profile picture 23

---
+++ src/functions/user.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export const createPost = async (
+    url,
+    token
+) => {
+    try {
+        const {data} = await axios.put(`${process.env.REACT_APP_BACKEND_URL}/updateProfilePicture`, {
+            url
+        }, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        });
+        console.log("Data: ", data);
+        return "ok";
+    } catch (e) {
+        return e.response.data.message;
+    }
+};
