cae76b7dfb8c9f56b326755aaacdc4284bc0e8b2
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-25 23:42:49 +0900

    #109 Finish update profile picture, loader, visibility, live change 18

--- src/components/profilePicture/UpdateProfilePicture.js
+++ src/components/profilePicture/UpdateProfilePicture.js
@@ -74,10 +74,10 @@ export default function UpdateProfilePicture({image, setImage, setError, setShow
                     setLoading(false);
                     setImage("");
                     pRef.current.style.backgroundImage = `url(${res[0].url})`;
-                    Cookies.set("user", {
+                    Cookies.set("user", JSON.stringify({
                         ...user,
                         picture: res[0].url,
-                    });
+                    }));
                     dispatch({
                         type: "UPDATEPICTURE",
                         payload: res[0].url,
