e43cb2bd32ab5e12b47aed1a33185acd1da528d6
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-27 22:30:48 +0900

    #113<2> Update cover picture 4

--- src/functions/user.js
+++ src/functions/user.js
@@ -18,3 +18,22 @@ export const updateprofilePicture = async (
         return e.response.data.message;
     }
 };
+
+export const updateCover = async (
+    url,
+    token
+) => {
+    try {
+        const {data} = await axios.put(`${process.env.REACT_APP_BACKEND_URL}/updateCover`, {
+            url
+        }, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        });
+        console.log("Data: ", data);
+        return "ok";
+    } catch (e) {
+        return e.response.data.message;
+    }
+};
