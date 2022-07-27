c8b8074b0bd30af24d9c45d068a641bad9833273
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-27 22:02:47 +0900

    #112 Update cover picture 23

--- src/pages/profile/Cover.js
+++ src/pages/profile/Cover.js
@@ -60,9 +60,17 @@ export default function Cover({cover, visitor}) {
 
     useEffect(() => {
         setWidth(coverRef.current.clientWidth);
+        /* Used to get the width of the browser window. */
     }, [window.innerWidth]);
     return (
         <div className="profile_cover" ref={coverRef}>
+            <div className="save_changes_cover">
+                <div className="save_changes_left">
+                    <i className="public_icon"></i>
+                    Your cover photo is public
+                </div>
+                <div className="save_changes_right"></div>
+            </div>
             <input
                 type="file"
                 ref={refInput}
