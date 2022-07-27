652bf8197e21fcaa7aed8b3fe821e68ed58077db
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-27 23:36:57 +0900

    #114 Select old cover pictures and update 4

--- src/pages/profile/OldCovers.js
+++ src/pages/profile/OldCovers.js
@@ -1,5 +1,13 @@
 export default function OldCovers() {
     return (
-        <div className="blur"></div>
+        <div className="blur">
+            <div className="postBox selectCoverBox">
+                <div className="box_header">
+                    <div className="small_circle">
+                        <i className="exit_icon"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
