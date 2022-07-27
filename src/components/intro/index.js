c514d1c757955eeeb237cdaf738783678bbefd2f
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-28 00:20:34 +0900

    #115 Display profile infos intro 4

--- src/components/intro/index.js
+++ src/components/intro/index.js
@@ -1,6 +1,9 @@
 import "./style.css";
+import {useState} from "react";
 
 export default function Intro() {
+    const initial = {};
+    const [infos, setInfos] = useState(initial);
     return (
         <div className="profile_card">
             <div className="profile_card_header">Intro</div>
