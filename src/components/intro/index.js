5d33e8cb03bd9900faf463dc8f0b3033e2645b1c
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-28 00:59:51 +0900

    #115 Display profile infos intro 10

--- src/components/intro/index.js
+++ src/components/intro/index.js
@@ -5,8 +5,8 @@ export default function Intro({details}) {
     const initial = {
         bio: details?.bio ? details.bio : "",
         otherName: details?.otherName ? details.otherName : "",
-        job: details?.job ? details.job : "Web developer!!",
-        workplace: details?.workplace ? details.workplace : "",
+        job: details?.job ? details.job : "Web developer",
+        workplace: details?.workplace ? details.workplace : "Google",
         highSchool: details?.highSchool ? details.highSchool : "",
         college: details?.college ? details.college : "",
         currentCity: details?.currentCity ? details.currentCity : "",
@@ -19,10 +19,10 @@ export default function Intro({details}) {
         <div className="profile_card">
             <div className="profile_card_header">Intro</div>
             {
-                infos.job && !infos.workplace && (
+                infos.job && infos.workplace && (
                     <div className="info_profile">
                         <img src="../../../icons/job.png" alt=""/>
-                        works as {infos.job}
+                        works as {infos.job} at <b>{infos.workplace}</b>
                     </div>
                 )
             }
