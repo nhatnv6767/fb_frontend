c9dbad7b5cf70a0fbe54ffebc3291cb1eb75ae13
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-29 00:35:50 +0900

    #118 Edit details 11

--- src/components/intro/Detail.js
+++ src/components/intro/Detail.js
@@ -1,5 +1,22 @@
 export default function Detail({header, img, value}) {
     return (
-        <div>Detail</div>
+        <div>
+            <div className="details_header">{header}</div>
+            <div className="add_details_flex no_underline">
+                {
+                    value ? <div className="info_profile">
+                            <img src={`../../../icons/${img}.png`} alt=""/>
+                            {value}
+                            <i className="edit_icon"></i>
+                        </div>
+                        : (
+                            <>
+                                <i className="rounded_plus_icon"></i>
+                                Add {header}
+                            </>
+                        )
+                }
+            </div>
+        </div>
     );
 }
