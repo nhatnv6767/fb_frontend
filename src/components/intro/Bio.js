72a458aa9fe7f0d44ba21eae1e3ad6175f55af33
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-28 21:21:14 +0900

    #116<2> Display profile infos intro 11

--- src/components/intro/Bio.js
+++ src/components/intro/Bio.js
@@ -1,11 +1,13 @@
-export default function Bio({infos}) {
+export default function Bio({infos, handleBioChange}) {
     return (
         <div className="add_bio_wrap">
             <textarea
                 placeholder="Add Bio"
                 name="bio"
                 value={infos?.bio}
+                maxLength="100"
                 className="textarea_blue details_input"
+                onChange={handleBioChange}
             >
 
             </textarea>
