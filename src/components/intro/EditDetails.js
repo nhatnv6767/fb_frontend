1d212c5c745676763fe02ace6e8e82d8fc0cd60a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-29 00:03:29 +0900

    #118 Edit details 5

--- src/components/intro/EditDetails.js
+++ src/components/intro/EditDetails.js
@@ -1,5 +1,14 @@
 export default function EditDetails() {
     return (
-        <div>EditDetails</div>
+        <div className="blur">
+            <div className="postBox infosBox">
+                <div className="box_header">
+                    <div className="small_circle">
+                        <i className="exit_icon"></i>
+                    </div>
+                    <span>Edit Details</span>
+                </div>
+            </div>
+        </div>
     );
 }
