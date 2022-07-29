5215d5950493c0a8e5d69a514c273354b94b2922
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-30 01:59:07 +0900

    #119<2> Edit details 11

--- src/components/intro/Detail.js
+++ src/components/intro/Detail.js
@@ -15,7 +15,10 @@ export default function Detail({
     return (
         <div>
             <div className="details_header">{header}</div>
-            <div className="add_details_flex ">
+            <div
+                className="add_details_flex "
+                onClick={() => setShow(true)}
+            >
                 {
                     value ? <div className="info_profile no_underline">
                             <img src={`../../../icons/${img}.png`} alt=""/>
@@ -23,10 +26,10 @@ export default function Detail({
                             <i className="edit_icon"></i>
                         </div>
                         : (
-                            <div onClick={() => setShow(true)}>
+                            <>
                                 <i className="rounded_plus_icon"></i>
                                 Add {header}
-                            </div>
+                            </>
                         )
                 }
             </div>
