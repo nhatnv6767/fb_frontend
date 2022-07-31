33814175e0a8e84684310cfd0809bb2ff287db98
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-31 22:46:17 +0900

    #126 Show the requests buttons 2

--- src/pages/profile/Friendship.js
+++ src/pages/profile/Friendship.js
@@ -1,5 +1,22 @@
 export default function Friendship() {
+    const friendship = {
+        friends: false,
+        following: false,
+        requestSent: false,
+        requestReceived: false,
+    };
     return (
-        <div>Friendship</div>
+        <div>
+            {
+                friendship.friends ? (
+                    <div className="friends_menu_wrap">
+                        <button className="gray_btn">
+                            <img src="../../../friends.png" alt=""/>
+                            <span>Friends</span>
+                        </button>
+                    </div>
+                ) : ("")
+            }
+        </div>
     );
 }
