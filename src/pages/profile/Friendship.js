0e06c15245750bc1a6b0b511dc21cce32fac1798
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-01 21:39:37 +0900

    #126 Show the requests buttons 17

--- src/pages/profile/Friendship.js
+++ src/pages/profile/Friendship.js
@@ -71,7 +71,45 @@ export default function Friendship() {
                             <span>Cancel Request</span>
                         </button>
                     ) : (
-                        friendship.requestReceived && ("")
+                        friendship.requestReceived && (
+                            <div className="friends_menu_wrap">
+                                <button className="gray_btn" onClick={() => setFriendsMenu(true)}>
+                                    <img src="../../../icons/friends.png" alt=""/>
+                                    <span>Friends</span>
+                                </button>
+                                {
+                                    friendsMenu && (
+                                        <div className="open_cover_menu" ref={menu}>
+                                            <div className="open_cover_menu_item">
+                                                <img src="../../../icons/favoritesOutline.png" alt=""/>
+                                                Favorites
+                                            </div>
+                                            <div className="open_cover_menu_item">
+                                                <img src="../../../icons/editFriends.png" alt=""/>
+                                                Edit Friend list
+                                            </div>
+                                            {
+                                                friendship.following ? (
+                                                    <div className="open_cover_menu_item">
+                                                        <img src="../../../icons/unfollowOutlined.png" alt=""/>
+                                                        Unfollow
+                                                    </div>
+                                                ) : (
+                                                    <div className="open_cover_menu_item">
+                                                        <img src="../../../icons/unfollowOutlined.png" alt=""/>
+                                                        Follow
+                                                    </div>
+                                                )
+                                            }
+                                            <div className="open_cover_menu_item">
+                                                <i className="unfriend_outlined_icon"></i>
+                                                Unfriend
+                                            </div>
+                                        </div>
+                                    )
+                                }
+                            </div>
+                        )
 
                     )
             }
