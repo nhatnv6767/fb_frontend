d054e7ad7b767fe55db575902ca1b057291f8d7a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-01 22:06:58 +0900

    #127 Submit requests 1

--- src/functions/user.js
+++ src/functions/user.js
@@ -37,3 +37,21 @@ export const updateCover = async (
         return e.response.data.message;
     }
 };
+export const addFriend = async (
+    url,
+    token
+) => {
+    try {
+        const {data} = await axios.put(`${process.env.REACT_APP_BACKEND_URL}/updateCover`, {
+            url
+        }, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        });
+        console.log("Data: ", data);
+        return "ok";
+    } catch (e) {
+        return e.response.data.message;
+    }
+};
