7068a3bbb738274205d99bdf5302e9a9a2f6685c
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-01 22:12:56 +0900

    #127 Submit requests 4

--- src/functions/user.js
+++ src/functions/user.js
@@ -99,3 +99,51 @@ export const unfollow = async (
         return e.response.data.message;
     }
 };
+
+export const acceptRequest = async (
+    id,
+    token
+) => {
+    try {
+        const {data} = await axios.put(`${process.env.REACT_APP_BACKEND_URL}/acceptRequest/${id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        });
+        return "ok";
+    } catch (e) {
+        return e.response.data.message;
+    }
+};
+
+export const unfriend = async (
+    id,
+    token
+) => {
+    try {
+        const {data} = await axios.put(`${process.env.REACT_APP_BACKEND_URL}/unfriend/${id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        });
+        return "ok";
+    } catch (e) {
+        return e.response.data.message;
+    }
+};
+
+export const deleteRequest = async (
+    id,
+    token
+) => {
+    try {
+        const {data} = await axios.put(`${process.env.REACT_APP_BACKEND_URL}/deleteRequest/${id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        });
+        return "ok";
+    } catch (e) {
+        return e.response.data.message;
+    }
+};
