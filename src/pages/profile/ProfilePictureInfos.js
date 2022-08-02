20ce94c0b7aa28460ba7912baadb34349582652e
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-02 22:14:11 +0900

    #131 Display friends pictures preview 5

--- src/pages/profile/ProfilePictureInfos.js
+++ src/pages/profile/ProfilePictureInfos.js
@@ -56,7 +56,12 @@ export default function ProfilePictureInfos({profile, visitor, photos, othername
                     <div className="profile_friend_imgs">
                         {
                             profile?.friends && profile.friends.slice(0, 6).map((friend, i) => (
-                                <img src={friend.picture} key={i} alt=""/>
+                                <img
+                                    src={friend.picture}
+                                    key={i}
+                                    alt=""
+                                    style={{transform: `translateX(${-i * 7}px)`}}
+                                />
                             ))
                         }
                     </div>
