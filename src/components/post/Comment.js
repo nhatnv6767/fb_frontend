505989c065505117afb788636b4437d4a17e6ee5
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-04 01:49:53 +0900

    #138 List comments, load more, live update 8

--- src/components/post/Comment.js
+++ src/components/post/Comment.js
@@ -1,5 +1,7 @@
 export default function Comment({comment}) {
     return (
-        <div>Comment</div>
+        <div className="comment">
+            <img src={comment?.commentBy.picture} alt=""/>
+        </div>
     );
 }
