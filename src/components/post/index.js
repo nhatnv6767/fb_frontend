12780608981eba9581c38a9a73504591d8aa1e74
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-04 01:39:37 +0900

    #138 List comments, load more, live update 3

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -15,10 +15,11 @@ export default function Post({post, user, profile}) {
     const [reacts, setReacts] = useState();
     const [check, setCheck] = useState();
     const [total, setTotal] = useState(0);
+    const [comments, setComments] = useState([]);
     useEffect(() => {
         getPostReacts();
     }, [post]);
-    console.log(post);
+
     const getPostReacts = async () => {
         const res = await getReacts(post._id, user.token);
         setReacts(res.reacts);
