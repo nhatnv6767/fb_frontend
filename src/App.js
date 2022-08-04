ba20d1bf1c61f5ee7d5ad4d847c0f527c50975a0
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-04 22:35:42 +0900

    #139 Display posts for the people you follow and fix issues 1

--- src/App.js
+++ src/App.js
@@ -63,7 +63,17 @@ function App() {
                 <Route element={<LoggedInRoutes/>}>
                     <Route path="/profile" element={<Profile setVisible={setVisible}/>} exact/>
                     <Route path="/profile/:username" element={<Profile setVisible={setVisible}/>} exact/>
-                    <Route path="/" element={<Home setVisible={setVisible} posts={posts}/>} exact/>
+                    <Route
+                        path="/"
+                        element={
+                            <Home
+                                setVisible={setVisible}
+                                posts={posts}
+                                loading={loading}
+                            />
+                        }
+                        exact
+                    />
                     <Route path="/activate/:token" element={<Activate/>} exact/>
                 </Route>
                 <Route element={<NotLoggedInRoutes/>}>
