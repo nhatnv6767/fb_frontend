6bafa6cb4b088babb387504c567cacd5824d7fa2
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-06 16:19:50 +0900

    #141 Save, un-save post 14

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -17,6 +17,7 @@ export default function Post({post, user, profile}) {
     const [check, setCheck] = useState();
     const [total, setTotal] = useState(0);
     const [count, setCount] = useState(1);
+    const [checkSaved, setCheckSaved] = useState();
     const [comments, setComments] = useState([]);
 
     useEffect(() => {
