905e876a6e5dd33ead48697d86ac2f52af8f21c4
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-07 19:16:37 +0900

    #149 Friends page-header & left menu 14

--- src/pages/friends/index.js
+++ src/pages/friends/index.js
@@ -8,7 +8,7 @@ export default function Friends() {
             <div className="friends">
                 <div className="friends_left">
                     <div className="friends_left_header">
-                        <h2>Friends</h2>
+                        <h3>Friends</h3>
                         <div className="small_circle">
                             <i className="settings_filled_icon"></i>
                         </div>
