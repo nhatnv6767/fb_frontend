6e8fd878f7e3326e062b1013e038ed2710fdf5b7
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-07 21:58:34 +0900

    #152 Buttons functionalities 1

--- src/pages/friends/Card.js
+++ src/pages/friends/Card.js
@@ -1,18 +1,27 @@
 import {Link} from "react-router-dom";
+import {cancelRequest} from "../../functions/user";
 
-export default function Card({user, type}) {
+export default function Card({userr, type}) {
+    const cancelRequestHandler = async (userId) => {
+        await cancelRequest()
+    }
     return (
         <div className="req_card">
-            <Link to={`/profile/${user?.username}`}>
-                <img src={user?.picture} alt=""/>
+            <Link to={`/profile/${userr?.username}`}>
+                <img src={userr?.picture} alt=""/>
             </Link>
             <div className="req_name">
-                {user?.first_name} {user?.last_name}
+                {userr?.first_name} {userr?.last_name}
             </div>
             {
                 type === "sent" ?
                     (
-                        <button className="blue_btn">Cancel Request</button>
+                        <button
+                            className="blue_btn"
+                            onClick={() => cancelRequestHandler(userr?._id)}
+                        >
+                            Cancel Request
+                        </button>
                     )
                     : (
                         type === "requests" ?
