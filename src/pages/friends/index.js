0ea934bd39e84a158ba5a92fbae78a27faba542a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-07 22:49:21 +0900

    #153 Filter by menu and switch pages and active menu 8

--- src/pages/friends/index.js
+++ src/pages/friends/index.js
@@ -40,7 +40,7 @@ export default function Friends() {
                     </div>
                     <div className="friends_left_wrap">
                         <Link to="/friends" className="mmenu_item active_friends">
-                            <div className="small_circle" style={{background: "#1876f2"}}>
+                            <div className="small_circle">
                                 <i className="friends_home_icon invert"></i>
                             </div>
                             <span>Home</span>
