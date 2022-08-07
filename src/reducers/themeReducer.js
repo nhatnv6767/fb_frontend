bc53d46bf0a6357272a75f00f906b42f21c01edf
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-08 01:02:48 +0900

    #157 Switch between dark mode and light mode, fix issues 1

---
+++ src/reducers/themeReducer.js
@@ -0,0 +1,19 @@
+import Cookies from "js-cookie";
+
+export function themeReducer(
+    state = Cookies.get("user")
+        ? JSON.parse(Cookies.get("user")) : null,
+    action) {
+    switch (action.type) {
+        case "LOGIN":
+            return action.payload;
+        case "LOGOUT":
+            return null;
+        case "UPDATEPICTURE":
+            return {...state, picture: action.payload};
+        case "VERIFY":
+            return {...state, verified: action.payload};
+        default:
+            return state;
+    }
+}
