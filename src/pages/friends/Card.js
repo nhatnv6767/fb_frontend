1d3b8e47227464afa8533ed5d227fe8eaf8025a9
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-07 21:22:25 +0900

    #151 Display all cards, responsive 5

--- src/pages/friends/Card.js
+++ src/pages/friends/Card.js
@@ -1,5 +1,14 @@
-export default function Card() {
+import {Link} from "react-router-dom";
+
+export default function Card({user}) {
     return (
-        <div>Card</div>
+        <div className="req_card">
+            <Link to={`/profile/${user?.username}`}>
+                <img src={user?.picture} alt=""/>
+            </Link>
+            <div className="req_name">
+                {user?.first_name} {user?.last_name}
+            </div>
+        </div>
     );
 }
