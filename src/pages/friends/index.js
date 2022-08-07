64d0e9a3f6267a954a945ce6979087750afb334c
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-07 14:00:12 +0900

    #149 Friends page-header & left menu 4

--- src/pages/friends/index.js
+++ src/pages/friends/index.js
@@ -1,7 +1,10 @@
 import "./style.css";
+import Header from "../../components/header";
 
 export default function Friends() {
     return (
-        <div>Friends</div>
+        <>
+            <Header page="friends"/>
+        </>
     );
 }
