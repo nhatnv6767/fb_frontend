f4aa81354bb32633882b7de04ca4ee78baa79285
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-09 03:51:22 +0900

    #159<2> Skeleton loader effect 8

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -254,13 +254,35 @@ export default function Profile({getAllPosts}) {
                         }`}>
                             <div className="profile_left" ref={leftSide}>
                                 {
-                                    true ?
+                                    loading ?
                                         <>
+                                            <div className="profile_card">
+                                                <div className="profile_card_header">
+                                                    Intro
+
+                                                </div>
+                                                <div className="sekelton_loader">
+                                                    <HashLoader color="#1876f2"/>
+                                                </div>
+                                            </div>
+
                                             <div className="profile_card">
                                                 <div className="profile_card_header">
                                                     Photos
                                                     <div className="profile_header_link">
                                                         See all photos
+                                                    </div>
+                                                </div>
+                                                <div className="sekelton_loader">
+                                                    <HashLoader color="#1876f2"/>
+                                                </div>
+                                            </div>
+
+                                            <div className="profile_card">
+                                                <div className="profile_card_header">
+                                                    Friends
+                                                    <div className="profile_header_link">
+                                                        See all friends
                                                     </div>
                                                 </div>
                                                 <div className="sekelton_loader">
