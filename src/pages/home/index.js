6c533a0666b32f8a9ecfeebcd671063241465c0c
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-09 03:55:51 +0900

    #159<2> Skeleton loader effect 10

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -8,6 +8,7 @@ import "./style.css";
 import CreatePost from "../../components/createPost";
 import SendVerification from "../../components/home/sendVerification";
 import Post from "../../components/post";
+import {HashLoader} from "react-spinners";
 
 export default function Home({setVisible, posts, loading, getAllPosts}) {
     const {user} = useSelector((state) => ({...state}));
@@ -28,13 +29,22 @@ export default function Home({setVisible, posts, loading, getAllPosts}) {
                 }
 
                 <CreatePost user={user} setVisible={setVisible}/>
-                <div className="posts">
-                    {
-                        posts.map((post, i) => (
-                            <Post key={i} post={post} user={user}/>
-                        ))
-                    }
-                </div>
+                {
+                    loading ? (
+                        <div className="sekelton_loader">
+                            <HashLoader color="#1876f2"/>
+                        </div>
+                    ) : (
+                        <div className="posts">
+                            {
+                                posts.map((post, i) => (
+                                    <Post key={i} post={post} user={user}/>
+                                ))
+                            }
+                        </div>
+                    )
+                }
+
             </div>
             <RightHome user={user}/>
         </div>
